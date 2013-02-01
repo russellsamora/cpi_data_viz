81adc3593f2e1b2c07af0d926a735170910f53de
russell goldenberg <russellgoldenberg@gmail.com>
2013-02-01 16:15:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+CPI data visualization
+================
+
---
+++ js/viz.js
@@ -0,0 +1,10 @@
+var currentInfo = null,
+    globalInfo = null,
+    vizContainer = null;
+
+$(function() {
+
+    currentInfo = $('.currentInfo');
+    globalInfo = $('.globalInfo');
+    vizContainer = $('.vizContainer');
+});
